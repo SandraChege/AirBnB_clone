a35d36ba453f0e5134ad790c2f9349f5bbc2a6a2
Nima <49175722+nima-umaima@users.noreply.github.com>
2023-02-14 12:22:44 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-"""
-Amenity class, a subclass of BaseModel
-"""
+""" Class Amenity """
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """
-    A subclass of BaseModel class
-    Public class attribute:
-        name: (str)
-    """
+    """Amenity class that inherits BaseModel"""
     name = ""
