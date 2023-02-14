08eec4d97efe23e64cc41f1c952d1fb55d10de3a
Nima <49175722+nima-umaima@users.noreply.github.com>
2023-02-14 12:25:29 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-"""
-State class, a subclass of BaseModel
-"""
+""" Class State """
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """
-    A subclass of BaseModel class
-    Public class attribute:
-        name: (str)
-    """
+    """ State class that inherits BaseModel"""
     name = ""
