143e174c5c182d1736d7d9cd8e2bdb0180a6b43f
Nima <49175722+nima-umaima@users.noreply.github.com>
2023-02-14 12:24:12 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""
-City class, a subclass of BaseModel
-"""
+""" Class City """
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """
-    A subclass of BaseModel class
-    Public class attributes:
-        state_id: (str) will be State.id
-        name:     (str)
-    """
+    """ City class that inherits BaseModel """
     state_id = ""
     name = ""
